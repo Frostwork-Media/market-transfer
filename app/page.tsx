2db811379e63021124b03efddb59aaa1d271bfab
nathanpmyoung <nathanpmyoung@gmail.com>
2023-03-28 11:52:28 +0100

    multiple changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,19 @@
 'use client';
-import Form from '../components/Form'
-export default function Home() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../components/Home';
+import NewPage from '../components/NewPage';
 
+const App: React.FC = () => {
   return (
-    <main className=''>
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <div className="h-screen flex justify-center">
-          <div className="flex flex-col">
-            <div className="relative w-full my-auto">
-              <h1 className="text-6xl font-bold text-center py-4">Prediction Market Mover</h1>
-              <h2 className="text-2xl font-bold text-center py-4">Move a prediction market from one site to another</h2>
-              <div className="w-full max-w-md px-4">
-                <Form />
-              </div>
-              <a href="https://chat.whatsapp.com/DKKQ5wESCOHGeN5nCFVotI" target='_blank' className="bg-green-500 hover:bg-green-700  font-bold py-2 px-4 rounded-full mt-8">Say hi👋 or report bugs🐛 (whatsapp chat)</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-page" element={<NewPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
