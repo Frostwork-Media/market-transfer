9cce421883745017084c5792557bd63146a1ded0
tom-turner <72453954+tom-turner@users.noreply.github.com>
2023-04-03 09:55:08 +0000

    Co-authored-by: Nathan P. M. Young <Nathanpmyoung@users.noreply.github.com>

--- app/api/placeBet/route.ts
+++ app/api/placeBet/route.ts
@@ -1,8 +1,10 @@
-//import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const body = await request.json();
+  
   console.log('placeBet called with body:', body.apiKey, body.betAmount, body.outcomeToBuy, body.marketID);
+  
   const res = await fetch('https://manifold.markets/api/v0/bet', {
     method: 'POST',
     headers: {
@@ -17,10 +19,14 @@ export async function POST(request: Request) {
   });
 
   if (!res.ok) {
-    const errorDetails = await res.text();
-    console.log('Error details:', errorDetails);
+    const errorDetails = await res.json();
+    console.error(errorDetails);
+    return NextResponse.error();
   }
 
-  console.log(res);
-  return new Response('placeBet called', { status: 200 });
+  const data = await res.json();
+
+  console.log(data)
+
+  return NextResponse.json(data)
 }
--- lib/api.ts
+++ lib/api.ts
@@ -1,24 +1,28 @@
 export function getMarketBySlug(slug) {
-    console.log(`Fetching market with slug ${slug}`);
-    return fetch(`https://manifold.markets/api/v0/slug/${slug}`).then(res => res.json()); 
+  console.log(`Fetching market with slug ${slug}`);
+  return fetch(`https://manifold.markets/api/v0/slug/${slug}`).then(res => res.json());
 }
 
 export function getIDBySlug(slug) {
-    return  getMarketBySlug(slug).then(market => market.id);
+  return getMarketBySlug(slug).then(market => market.id);
 }
 
-const placeBet = (apiKey, marketID, betAmount, outcomeToBuy) => {
-    return fetch('/placeBet', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ apiKey, marketID, betAmount, outcomeToBuy }),
-    });
-  };
+export function placeBet(apiKey, marketID, betAmount, outcomeToBuy){
+  return fetch('/api/placeBet', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ apiKey, marketID, betAmount, outcomeToBuy }),
+  });
+};
+
+// place bet specifically by slug
 
 export async function placeBetBySlug(apiKey, slug, betAmount, outcomeToBuy) {
-    const marketID = await getIDBySlug(slug);
-    console.log(`marketID is ${marketID}`);
-    await placeBet(apiKey, marketID, betAmount, outcomeToBuy);
+  const marketID = await getIDBySlug(slug);
+  console.log(`marketID is ${marketID}`);
+  await placeBet(apiKey, marketID, betAmount, outcomeToBuy);
 }
+
+// place bet by other things
