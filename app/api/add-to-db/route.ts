e70d6cf9f8ebf2c21a73f6fd91f9a49288e6a1f7
tom-turner <tom.turner@live.co.uk>
2023-05-22 10:47:45 +0100

    added test routes

---
+++ app/api/add-to-db/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../lib/prisma';
+// NEED TO USE API PAGE ROUTE DUE TO CORS LIMITATIONS ON CLIENT SIDE
+
+export async function POST(request: Request) {
+
+    const test = await prisma.test.create({
+        data: {
+            name: "test"
+        }
+    })
+
+    console.log(test)
+
+    return NextResponse.json(test)
+}
