4e8b888eb9c40e180049590a0af4c9068bac18d2
Rob Gordon <rchristopher.gordon@gmail.com>
2023-05-25 17:19:56 -0400

    Update link to probability graph

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const pages = [
-  { name: 'Automate Betting', href: '/' },
-  { name: 'Market Creator', href: '/market-creator' },
-  { name: 'Market Mover', href: '/market-mover' },
-  { name: 'GPT4Casting', href: '/gpt4casting' },
-  { name: 'EML', href: 'https://engage-mazurka-lutanist.vercel.app/' },
+  { name: "Automate Betting", href: "/" },
+  { name: "Market Creator", href: "/market-creator" },
+  { name: "Market Mover", href: "/market-mover" },
+  { name: "GPT4Casting", href: "/gpt4casting" },
+  { name: "EML", href: "https://probability-graph.predictionlab.org/" },
 ];
 
 export default function Navbar() {
   const [activePage, setActivePage] = useState(null);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       setActivePage(window.location.pathname);
     }
   }, []);
@@ -27,7 +27,9 @@ export default function Navbar() {
           <a
             key={i}
             href={page.href}
-            className={`capitalize ${isActive ? 'bg-blue-400 rounded-lg px-2 text-white' : ''}`}
+            className={`capitalize ${
+              isActive ? "bg-blue-400 rounded-lg px-2 text-white" : ""
+            }`}
           >
             {page.name}
           </a>
